063a3ad945d8f6304051d598c0ea8a9973376e09
Pa345-ai <parameeranasinghe33@gmail.com>
2025-12-19 17:19:03 +0530

    Create Iam.tf

---
+++ terrafirm/Iam.tf
@@ -0,0 +1,54 @@
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name               = "${var.project_name}-${var.environment}-ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "ecs_execution_secrets" {
+  name = "${var.project_name}-${var.environment}-secrets-execution-policy"
+  role = aws_iam_role.ecs_task_execution.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["secretsmanager:GetSecretValue", "kms:Decrypt"]
+        Effect   = "Allow"
+        Resource = [aws_secretsmanager_secret.app_secrets.arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ecs_task" {
+  name               = "${var.project_name}-${var.environment}-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+}
+
+resource "aws_iam_role_policy" "ecs_task_logs" {
+  name = "${var.project_name}-${var.environment}-task-logs-policy"
+  role = aws_iam_role.ecs_task.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["logs:CreateLogStream", "logs:PutLogEvents"]
+        Effect = "Allow"
+        Resource = ["${aws_cloudwatch_log_group.ecs.arn}:*"]
+      }
+    ]
+  })
+}
