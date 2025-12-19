80829a5278eee406b7880cee4dcdbe75ba9c3a1b
Pa345-ai <parameeranasinghe33@gmail.com>
2025-12-19 17:13:56 +0530

    Create output.tf

---
+++ terrafirm/output.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_url" {
+  description = "Full URL to access the application"
+  value       = "http://${aws_lb.main.dns_name}"
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.main.name
+}
+
+output "cloudwatch_log_group" {
+  description = "CloudWatch log group for ECS logs"
+  value       = aws_cloudwatch_log_group.ecs.name
+}
