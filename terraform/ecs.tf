ef68ed8389a1d5233e9136a1759888240a15c52d
Pa345-ai <parameeranasinghe33@gmail.com>
2025-12-19 21:07:11 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -11,7 +11,7 @@ resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-${var.environment}-cluster"
   setting {
     name  = "containerInsights"
-    value = "enabled" # Fixes tfsec Result #9
+    value = "enabled"
   }
   tags = {
     Name = "${var.project_name}-${var.environment}-cluster"
