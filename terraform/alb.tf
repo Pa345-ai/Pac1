f1dc2c4ab7ab4249ef1af20f63ec41761b7969a7
Pa345-ai <parameeranasinghe33@gmail.com>
2025-12-19 20:43:52 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,11 +1,12 @@
 resource "aws_lb" "main" {
   name               = "${var.project_name}-${var.environment}-alb"
-  internal           = false
+  internal           = false # tfsec:ignore:aws-elb-alb-not-public
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
   subnets            = aws_subnet.public[*].id
 
   enable_deletion_protection = false
+  drop_invalid_header_fields = true # Fixes tfsec Result #5
 
   tags = {
     Name = "${var.project_name}-${var.environment}-alb"
@@ -40,10 +41,11 @@ resource "aws_lb_target_group" "main" {
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "HTTP" # tfsec:ignore:aws-elb-http-not-used
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
   }
 }
+
