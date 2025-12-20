7e22bbc7dfb7e10f7a79623ea4d56b26331169a1
Pa345-ai <parameeranasinghe33@gmail.com>
2025-12-20 12:54:48 +0530

    Update secrets.tf

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -2,10 +2,77 @@
 # KMS KEY FOR SECRETS
 ############################################
 
+data "aws_iam_policy_document" "secrets_kms_policy" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow ECS Task Execution Role to Decrypt Secrets"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.ecs_task_execution.arn]
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:GenerateDataKey"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow Secrets Manager to Use Key"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["secretsmanager.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+      "kms:CreateGrant"
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["secretsmanager.${var.aws_region}.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_kms_key" "secrets" {
   description             = "KMS key for application secrets"
   deletion_window_in_days = 7
   enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.secrets_kms_policy.json
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-secrets-kms"
+  }
+}
+
+resource "aws_kms_alias" "secrets" {
+  name          = "alias/${var.project_name}-${var.environment}-secrets"
+  target_key_id = aws_kms_key.secrets.key_id
 }
 
 ############################################
@@ -40,13 +107,25 @@ resource "aws_secretsmanager_secret_version" "app_secrets" {
 
 ############################################
 # IAM POLICY DOCUMENT FOR SECRET ACCESS
+# (Used by iam.tf for the execution role)
 ############################################
 
 data "aws_iam_policy_document" "secrets_access" {
   statement {
-    effect    = "Allow"
-    actions   = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
     resources = [aws_secretsmanager_secret.app_secrets.arn]
   }
-}
 
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey"
+    ]
+    resources = [aws_kms_key.secrets.arn]
+  }
+}
