c264d0a7927e1de507d446f5d6b65d35d69bd8b8
Pa345-ai <parameeranasinghe33@gmail.com>
2025-12-21 12:44:08 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,3 +1,9 @@
+################################################################################
+# AWS Terraform Starter Kit
+# Copyright (c) 2025 RUWANPURAGE PAVITHRA PARAMI RANASINGHE
+# Licensed for single commercial use - See LICENSE.txt
+################################################################################
+
 resource "aws_lb" "main" {
   name               = "${var.project_name}-${var.environment}-alb"
   internal           = false # tfsec:ignore:aws-elb-alb-not-public
